6e3f9b54676debb8207b01b77a8917dd90db160c
Samirmeshram <124778928+Samirmeshram@users.noreply.github.com>
2025-12-26 18:07:57 +0530

    config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "29895250"))
 API_HASH = environ.get("API_HASH", "29ca1e2311efdf950eea03a6ae2bc8ee")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7286166537:AAGnMaXBoT-fJsUXx2mNtBzARpYcmtF76uk")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', '')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2017335429').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "DP_CINEMA_FILE_BOT") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "SecureFileStoreBot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -35,7 +35,7 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://Dpcinema:Dpcinema@atlascluster.mcfyzy4.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
+DB_URI = environ.get("DB_URI", "")
 DB_NAME = environ.get("DB_NAME", "Dpcinema")
 
 # Auto Delete Information
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001962672428"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002386866175"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
